83ab03411d65bad081a1d3f9bfe93c7a28484e2e
Alina <alinakruzhkova21@gmail.com>
2026-01-15 15:13:50 +0200

    dz

---
+++ 3.py
@@ -0,0 +1,11 @@
+number = int (input('введите число от 1 до 5'))
+if number == 1:
+    print('Дуже погано')
+elif number ==2:
+    print('Погано')
+elif number ==3:
+    print('Задовільно')
+elif number ==4:
+    print('Добре')
+elif number ==5:
+    print('Відмінно')
