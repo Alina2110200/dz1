bac93e64d9d76f77da504702fd0cc62ff00a2b0e
Alina <alinakruzhkova21@gmail.com>
2026-01-15 15:02:29 +0200

    dz

--- 2.py
+++ 2.py
@@ -3,21 +3,21 @@ number2 = int (input('введите второе число'))
 number3 = int (input('введите третье число'))
 
 action = input('какое число вы хотите узнать? самое большое / самое маленькое / среднее')
-if action == 'самое большое':
+if action == ' самое большое':
     if number1 > number2 and number1 > number3:
         print(number1)
     elif number2 > number1 and number2 > number3:
         print(number2)
     else:
         print(number3)
-elif action == 'самое маленькое':
+elif action == ' самое маленькое':
     if number1 < number2 and number1 < number3:
         print(number1)
     elif number2 < number1 and number2 < number3:
         print(number2)
     else:
         print(number3)
-elif action == 'среднее':
+elif action == ' среднее':
     average = (number1 + number2 + number3) / 3
     print(average)
 
